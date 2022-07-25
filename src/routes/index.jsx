1527305d64fbbc2496e17cccf8dc796fcdfdd048
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-25 19:56:15 -0300

    feat: Adicionando página de produto com slug personalizado.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,14 @@
 import { Route, Switch } from "react-router-dom";
 
 import Home from "../pages/Home";
+import Product from "../pages/Product";
 
 const Routes = () => {
 
     return (
         <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/:slug" component={Product} />
         </Switch>
     )
 }
