2d4dbcafa2abb27de99fa00d83e0b7f33a9c964a
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-25 19:57:11 -0300

    feat: Providers na raíz do projeto para funcionar Context API.

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,14 @@ import "./styles/GlobalStyle.scss"
 import { BrowserRouter } from "react-router-dom";
 
 import App from './App'
+import Providers from './providers';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Providers>
+        <App />
+      </Providers>
     </BrowserRouter>
   </React.StrictMode>
 )
