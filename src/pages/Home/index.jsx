96a2674f787d95adeba979ee6f6b60256d336bde
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-26 09:01:03 -0300

    feat: Atribuição de classes e inserção de novos itens para melhor visualização.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,5 @@
+import "./styles.scss"
+
 import { Link } from "react-router-dom";
 import { useProducts } from "../../providers/Products";
 
@@ -8,23 +10,47 @@ function Home() {
     const { listOfProducts } = useProducts()
 
     return (
-        <>
-            <h1>Deliverize</h1>
+        <main className="homePageContainer">
+            <h2 className="homePageContainer__title">Confira nossos produtos</h2>
             {listOfProducts ? (
-                <>
+                <div className="homePageContainer__ProductList">
                     {listOfProducts.map((product) => (
                         <Link
                             to={`/${product.nm_product}`}
                             key={product.id}
+                            className="ProductList__Product"
                         >
                             <img src={product.url_image} alt={product.nm_product} />
                             <strong>{product.nm_product}</strong>
                             <span>R$ {product.vl_discount}</span>
                         </Link>
                     ))}
-                </>
+
+                    <Link
+                        className="ProductList__Product"
+                    >
+                        <img src="https://pbs.twimg.com/profile_images/1362579947479515145/rqw8d34L_400x400.jpg" />
+                        <strong>Outro Produto</strong>
+                        <span>R$ 10</span>
+                    </Link>
+
+                    <Link
+                        className="ProductList__Product"
+                    >
+                        <img src="https://pbs.twimg.com/profile_images/1362579947479515145/rqw8d34L_400x400.jpg" />
+                        <strong>Outro Produto</strong>
+                        <span>R$ 10</span>
+                    </Link>
+                    <Link
+                        className="ProductList__Product"
+                    >
+                        <img src="https://pbs.twimg.com/profile_images/1362579947479515145/rqw8d34L_400x400.jpg" />
+                        <strong>Outro Produto</strong>
+                        <span>R$ 10</span>
+                    </Link>
+                </div>
             ) : <ReactLoading type={"spin"} color={"red"} height={'20%'} width={'20%'} />}
-        </>
+        </main>
     )
 }
 
